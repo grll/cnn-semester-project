83b20ab1cb60728ef856428b9f2c32abc8b49908
Guillaume Raille <guillaume.raille@gmail.com>
2017-11-30 23:38:43 +0100

    first working version of the script runned

--- scripts/process_bleu_on_24_cpu.sh
+++ scripts/process_bleu_on_24_cpu.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+# script to run process_bleu_score.py in the background on the 24 CPU
+# @args:
+# - $1: number of images (to split in 24 bins)
+# - $2: name of the output file (gonna be filled with each bean mean bleu score comma separated)
+# - $3: path of the match_index file used by the python script to retrieve the match betweem images
+
 START=$(date +%s)
 let n=$1/24+1
 let c=0
 for ((i=0;i<$1;i+=n)); do
-    let c=c+1
     let end=$i+n
     if ((end>=$1)); then
         let end=$1
     fi
 
-    echo "python process_bleu_score $2 $3 $i $end"
+    python process_bleu_score.py $2 $3 $i $end &
+    pid_array[$c]=$!
+    let c=c+1
 done
-END=$(date +%s)
-DIFF=$(( $END - $START ))
-echo "It took $DIFF seconds"
